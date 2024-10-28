79f3e099411ee9d2d5692235c1ddc2a74affa01f
Collin Kleest <collinkleest@gmail.com>
2024-10-28 19:58:53 -0400

    feat: remove about page

--- app/_content/header.ts
+++ app/_content/header.ts
@@ -5,10 +5,6 @@ export const header: IHeader = {
   logoHref: '/',
   navLinks: [
     {
-      text: 'About',
-      href: '/about'
-    },
-    {
       text: 'Experience',
       href: '/experience'
     },
