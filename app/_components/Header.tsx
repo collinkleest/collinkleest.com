46a57f5445e6254114ecd855b1a55a0bd42eec2a
Collin Kleest <collinkleest@gmail.com>
2025-01-08 12:44:59 -0500

    chore(deps): remove tailwind

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -2,12 +2,8 @@
 import { Menu } from '@mui/icons-material'
 import content from '../_content/'
 import { useState } from 'react'
-import { usePathname } from 'next/navigation'
-import { Component } from 'react'
-import { ReactElement } from 'react'
 
 const Header = () => {
-  const pathname = usePathname()
   const [isOpen, setIsOpen] = useState(false)
 
   const toggleMobile = () => {
@@ -16,25 +12,16 @@ const Header = () => {
 
   return (
     <header>
-      <nav className="bg-slate-950 border-b-2 p-4 flex items-center sticky w-full justify-between flex-wrap t-0 z-10 font-mono">
-        <a href={content.header.logoHref || '/'} className="text-slate-300">
-          {content.header.logoText}
-        </a>
-        <div className="lg:hidden">
-          <Menu
-            onClick={toggleMobile}
-            className="text-slate-500 hover:text-slate-300"
-          />
+      <nav>
+        <a href={content.header.logoHref || '/'}>{content.header.logoText}</a>
+        <div>
+          <Menu onClick={toggleMobile} />
         </div>
-        <div className={`${isOpen ? '' : 'hidden'}  lg:flex`}>
-          <ul className="list-reset flex list-none">
+        <div>
+          <ul>
             {content.header.navLinks.map((navLink) => (
-              <li className="mr-3" key={navLink.text}>
-                <a
-                  href={navLink.href}
-                  className={`${pathname === navLink.href ? 'text-slate-300' : 'text-slate-500'} hover:text-slate-300`}>
-                  {navLink.text}
-                </a>
+              <li key={navLink.text}>
+                <a href={navLink.href}>{navLink.text}</a>
               </li>
             ))}
           </ul>
