67760c56324520016bb29bde8429b4a2a8b6d56e
Collin Kleest <collinkleest@gmail.com>
2025-01-07 23:57:37 -0500

    feat: remove testing deps and tests from repo

--- app/_content/metadata.ts
+++ app/_content/metadata.ts
@@ -17,5 +17,27 @@ export const metadata: Metadata = {
     template: '%s - Collin Kleest',
     default: 'Collin Kleest - Tech Lead'
   },
-  description: 'Software Engineer based in Philadelphia'
+  description: 'Software Engineer based in Philadelphia',
+  generator: 'Next.js',
+  applicationName: 'collinkleest.com',
+  referrer: 'origin-when-cross-origin',
+  keywords: [
+    'Collin Kleest',
+    'Portfolio',
+    'Personal Portfolio',
+    'Software Engineer',
+    'Tech Lead',
+    'Developer',
+    'Next.js',
+    'React',
+    'JavaScript'
+  ],
+  authors: [{ name: 'Collin Kleest', url: 'https://collinkleest.com' }],
+  creator: 'Collin Kleest',
+  publisher: 'Collin Kleest',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false
+  }
 }
