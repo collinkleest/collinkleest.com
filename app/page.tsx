f668e4e982d4bc63f4dd6938e29317d510174edc
Collin Kleest <collinkleest@gmail.com>
2025-01-13 10:55:45 -0500

    feat: clean up margins

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
-import { Container } from '@chakra-ui/react'
-import { Header, Intro, Timeline } from './_components'
+import { Box, Container } from '@chakra-ui/react'
+import { Intro, Timeline } from './_components'
 
 export default function Page() {
   return (
     <>
       <Container>
-        <Intro />
-        <Timeline />
+        <Box my={12}>
+          <Intro />
+        </Box>
+        <Box my={12}>
+          <Timeline />
+        </Box>
       </Container>
     </>
   )
