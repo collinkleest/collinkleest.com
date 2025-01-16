18f03afb9e5d593a5600c635ef1a51fdc87910c7
Collin Kleest <collinkleest@gmail.com>
2025-01-16 12:51:56 -0500

    ci(cd): deploy env for output dir

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from 'next'
 
+const DEPLOY_ENV: string | undefined = process?.env?.DEPLOY_ENV
+
 const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ['@chakra-ui/react']
   },
-  basePath:
-    process?.env?.DEPLOY_ENV === 'github-pages' ? '/collinkleest.com' : ''
+  basePath: DEPLOY_ENV === 'github-pages' ? '/collinkleest.com' : '',
+  output: DEPLOY_ENV === 'github-pages' ? 'export' : undefined
 }
 
 export default nextConfig
