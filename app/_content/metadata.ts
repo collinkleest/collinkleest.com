56e49d7489a11aeddab6dead8715e208e614bc26
Collin Kleest <collinkleest@gmail.com>
2025-01-21 22:42:37 -0500

    refactor(metadata): remove extra unused page metadata

--- app/_content/metadata.ts
+++ app/_content/metadata.ts
@@ -1,17 +1,5 @@
 import { Metadata } from 'next'
 
-export const experienceMetadata: Metadata = {
-  title: 'Experience'
-}
-
-export const projectsMetadata: Metadata = {
-  title: 'Projects'
-}
-
-export const resumeMetadata: Metadata = {
-  title: 'Resume'
-}
-
 export const metadata: Metadata = {
   title: {
     template: '%s - Collin Kleest',
