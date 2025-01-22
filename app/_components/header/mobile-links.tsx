e1e214ed5cb8c7117f9fb38778771201a8b58588
Collin Kleest <collinkleest@gmail.com>
2025-01-21 23:07:24 -0500

    refactor(header): clean up header nav links and mobile links with content

--- app/_components/header/mobile-links.tsx
+++ app/_components/header/mobile-links.tsx
@@ -1,8 +1,5 @@
+import { header } from '@_content'
 import { Box, Button, Stack, Text } from '@chakra-ui/react'
-import { HiBeaker } from 'react-icons/hi'
-import { IoIosDocument } from 'react-icons/io'
-import { IoPerson } from 'react-icons/io5'
-import { MdWork } from 'react-icons/md'
 
 export const MobileLinks = ({ isOpen }: { isOpen: boolean }) => {
   return (
@@ -10,39 +7,18 @@ export const MobileLinks = ({ isOpen }: { isOpen: boolean }) => {
       {isOpen && (
         <Box display={{ base: 'block', md: 'none' }}>
           <Stack direction={{ base: 'column', md: 'row' }}>
-            <a href="#about">
-              <Button size={'sm'} variant={'ghost'}>
-                <IoPerson />
-                <Text textStyle="sm" fontWeight={'bold'}>
-                  About
-                </Text>
-              </Button>
-            </a>
-            <a href="#experience">
-              <Button size={'sm'} variant={'ghost'}>
-                <MdWork />
-
-                <Text textStyle="sm" fontWeight={'bold'}>
-                  Experience
-                </Text>
-              </Button>
-            </a>
-            <a href="#resume">
-              <Button size={'sm'} variant={'ghost'}>
-                <IoIosDocument />
-                <Text textStyle="sm" fontWeight={'bold'}>
-                  Resume
-                </Text>
-              </Button>
-            </a>
-            <a href="#projects">
-              <Button size={'sm'} variant={'ghost'}>
-                <HiBeaker />
-                <Text textStyle="sm" fontWeight={'bold'}>
-                  Projects
-                </Text>
-              </Button>
-            </a>
+            {header?.navLinks?.map((navLink) => {
+              return (
+                <a key={navLink.text} href={navLink.href}>
+                  <Button size={'sm'} variant={'ghost'}>
+                    {navLink.icon}
+                    <Text textStyle="sm" fontWeight={'bold'}>
+                      {navLink.text}
+                    </Text>
+                  </Button>
+                </a>
+              )
+            })}
           </Stack>
         </Box>
       )}
--- app/_components/header/nav-links.tsx
+++ app/_components/header/nav-links.tsx
@@ -1,3 +1,4 @@
+import { header } from '@_content'
 import { Box, Button, Stack, Text } from '@chakra-ui/react'
 import { ColorModeButton } from '../../color-mode'
 
@@ -6,34 +7,17 @@ export const NavLinks = () => {
     <>
       <Box display={{ base: 'none', md: 'block' }}>
         <Stack direction={{ base: 'column', md: 'row' }} gap={4}>
-          <a href="#about">
-            <Button size={'sm'} variant={'ghost'}>
-              <Text textStyle="sm" fontWeight={'bold'}>
-                About
-              </Text>
-            </Button>
-          </a>
-          <a href="#experience">
-            <Button size={'sm'} variant={'ghost'}>
-              <Text textStyle="sm" fontWeight={'bold'}>
-                Experience
-              </Text>
-            </Button>
-          </a>
-          <a href="#resume">
-            <Button size={'sm'} variant={'ghost'}>
-              <Text textStyle="sm" fontWeight={'bold'}>
-                Resume
-              </Text>
-            </Button>
-          </a>
-          <a href="#projects">
-            <Button size={'sm'} variant={'ghost'}>
-              <Text textStyle="sm" fontWeight={'bold'}>
-                Projects
-              </Text>
-            </Button>
-          </a>
+          {header?.navLinks?.map((navLink) => {
+            return (
+              <a key={navLink.text} href={navLink.href}>
+                <Button size={'sm'} variant={'ghost'}>
+                  <Text textStyle="sm" fontWeight={'bold'}>
+                    {navLink.text}
+                  </Text>
+                </Button>
+              </a>
+            )
+          })}
         </Stack>
       </Box>
       <ColorModeButton display={{ base: 'none', md: 'inline-flex' }} />
