51b4a7660f509e0ca8382611de2e73d6d39fe882
Collin Kleest <collinkleest@gmail.com>
2025-01-24 18:27:56 -0500

    feat(logo): update with correct color mode

--- app/_components/header/logo.tsx
+++ app/_components/header/logo.tsx
@@ -1,15 +1,35 @@
+'use client'
+
 import { Box, Image } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+import { useColorMode } from '../../color-mode'
 
 export const Logo = () => {
+  const [currentMode, setCurrentMode] = useState('dark')
+  const mode = useColorMode()
+
+  useEffect(() => {
+    setCurrentMode(mode.colorMode)
+  }, [mode.colorMode])
+
   return (
     <Box marginEnd={'auto'}>
       <a href="#">
-        <Image
-          rounded="md"
-          src="images/CK_Logo_500x500_WHITE.png"
-          alt="Collin Kleest Logo"
-          boxSize="50px"
-        />
+        {currentMode == 'dark' ? (
+          <Image
+            rounded="md"
+            src="images/CK_Logo_500x500_WHITE.png"
+            alt="Collin Kleest Logo"
+            boxSize="50px"
+          />
+        ) : (
+          <Image
+            rounded="md"
+            src="images/CK_Logo_500x500.png"
+            alt="Collin Kleest Logo"
+            boxSize="50px"
+          />
+        )}
       </a>
     </Box>
   )
