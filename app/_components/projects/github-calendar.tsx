69034a1e4dfa92dddbcdcb9b5b7fc269bf427d18
Collin Kleest <collinkleest@gmail.com>
2025-01-28 22:48:03 -0500

    feat(github-contribution): begin year toggle implementation

--- app/_components/projects/github-calendar.tsx
+++ app/_components/projects/github-calendar.tsx
@@ -1,12 +1,32 @@
+'use client'
+
+import { Button, GridItem, SimpleGrid, VStack } from '@chakra-ui/react'
+import { useState } from 'react'
 import GitHubCalendar from 'react-github-calendar'
 import { ThemeType, useColorMode } from '../../color-mode'
 
 export const GithubCalendar = () => {
   const { colorMode } = useColorMode()
+  const [selectedYear, setSelectedYear] = useState(2025)
+
   return (
-    <GitHubCalendar
-      username="collinkleest"
-      colorScheme={colorMode as ThemeType}
-    />
+    <>
+      <SimpleGrid columns={8} gap={0}>
+        <GridItem colSpan={6}>
+          <GitHubCalendar
+            username="collinkleest"
+            colorScheme={colorMode as ThemeType}
+            year={selectedYear}
+          />
+        </GridItem>
+        <GridItem colSpan={2}>
+          <VStack>
+            <Button onClick={() => setSelectedYear(2025)}>2025</Button>
+            <Button onClick={() => setSelectedYear(2024)}>2024</Button>
+            <Button onClick={() => setSelectedYear(2023)}>2023</Button>
+          </VStack>
+        </GridItem>
+      </SimpleGrid>
+    </>
   )
 }
