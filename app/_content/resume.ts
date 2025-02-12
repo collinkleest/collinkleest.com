57b0d1c0efa0c7ddd0aec5170e038aca53906d08
Collin Kleest <collinkleest@gmail.com>
2025-02-11 23:48:01 -0500

    chore: add resume content

---
+++ app/_content/resume.ts
@@ -0,0 +1,13 @@
+export const resume = {
+  heading: 'Resume',
+  mainContentRichText: `My resume is built in LaTex and is from a template called
+        <a href="https://github.com/posquit0/Awesome-CV">
+          {' '}
+          <b>Awesome-CV</b>
+        </a>
+        . My resume is available for download in PDF format or you can view it
+        in a new tab.`,
+  viewInTabBtnText: 'View resume in new tab',
+  downloadBtnText: 'Download Resume PDF',
+  resumeHref: 'docs/resume.pdf'
+}
