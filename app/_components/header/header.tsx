3a4188c15c29d5306a0a3c3518aae94d1d4f6d7f
Collin Kleest <collinkleest@gmail.com>
2025-02-12 22:56:01 -0500

    feat(header): make mobile menu collapsible Refs #2

--- app/_components/header/header.tsx
+++ app/_components/header/header.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Box, Flex } from '@chakra-ui/react'
+import { Box, Collapsible, Flex } from '@chakra-ui/react'
 import { useState } from 'react'
 import { Logo } from './logo'
 import { MobileLinks } from './mobile-links'
@@ -11,16 +11,22 @@ export const Header = () => {
   const [isOpen, setIsOpen] = useState(false)
   return (
     <Box borderBottom={'1px solid #eaeaea'}>
-      <Flex
-        mx={{ base: 2, md: 30, lg: 50, xl: 70 }}
-        align={'center'}
-        justify={'between'}
-        py={4}>
-        <Logo />
-        <MobileToggle isOpen={isOpen} setIsOpen={setIsOpen} />
-        <NavLinks />
-      </Flex>
-      <MobileLinks isOpen={isOpen} />
+      <Collapsible.Root>
+        <Flex
+          mx={{ base: 2, md: 30, lg: 50, xl: 70 }}
+          align={'center'}
+          justify={'between'}
+          py={4}>
+          <Logo />
+          <Collapsible.Trigger>
+            <MobileToggle isOpen={isOpen} setIsOpen={setIsOpen} />
+          </Collapsible.Trigger>
+          <NavLinks />
+        </Flex>
+        <Collapsible.Content>
+          <MobileLinks />
+        </Collapsible.Content>
+      </Collapsible.Root>
     </Box>
   )
 }
--- app/_components/header/mobile-links.tsx
+++ app/_components/header/mobile-links.tsx
@@ -1,27 +1,25 @@
 import { header } from '@_content'
 import { Box, Button, Stack, Text } from '@chakra-ui/react'
 
-export const MobileLinks = ({ isOpen }: { isOpen: boolean }) => {
+export const MobileLinks = () => {
   return (
     <>
-      {isOpen && (
-        <Box display={{ base: 'block', md: 'none' }}>
-          <Stack direction={{ base: 'column', md: 'row' }}>
-            {header?.navLinks?.map((navLink) => {
-              return (
-                <a key={navLink.text} href={navLink.href}>
-                  <Button size={'sm'} variant={'ghost'}>
-                    {navLink.icon}
-                    <Text textStyle="sm" fontWeight={'bold'}>
-                      {navLink.text}
-                    </Text>
-                  </Button>
-                </a>
-              )
-            })}
-          </Stack>
-        </Box>
-      )}
+      <Box display={{ base: 'block', md: 'none' }}>
+        <Stack direction={{ base: 'column', md: 'row' }}>
+          {header?.navLinks?.map((navLink) => {
+            return (
+              <a key={navLink.text} href={navLink.href}>
+                <Button size={'sm'} variant={'ghost'}>
+                  {navLink.icon}
+                  <Text textStyle="sm" fontWeight={'bold'}>
+                    {navLink.text}
+                  </Text>
+                </Button>
+              </a>
+            )
+          })}
+        </Stack>
+      </Box>
     </>
   )
 }
