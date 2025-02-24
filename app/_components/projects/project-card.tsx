89f188f47c245edb0d099cb0227103ad3060bbe1
Collin Kleest <collinkleest@gmail.com>
2025-02-23 19:53:57 -0500

    feat(ui): clean up project cards and enhance ui

--- app/_components/projects/project-card.tsx
+++ app/_components/projects/project-card.tsx
@@ -1,5 +1,6 @@
 import { langMappings } from '@_content'
 import {
+  Box,
   Card,
   CardBody,
   CardDescription,
@@ -22,36 +23,77 @@ interface IProjectCardProps {
 }
 
 export const ProjectCard = (props: IProjectCardProps) => {
+  const handleCardClick = (githubLink: string, liveSiteLink?: string) => {
+    const url = liveSiteLink ?? githubLink
+    window.open(url, '_blank')
+  }
   return (
-    <Card.Root>
-      <CardHeader>
-        <Flex gap={2} align={'center'}>
-          {langMappings[props?.language?.toLowerCase()]}
-          {props?.language}
-        </Flex>
-      </CardHeader>
-      <CardBody>
-        <CardTitle>{props.name}</CardTitle>
-        <CardDescription>{props.description}</CardDescription>
-      </CardBody>
-      <CardFooter>
-        <a href={props.url} target="_blank">
-          <FaGithub />
-        </a>
-        {props.liveHomepage && (
-          <a href={props.liveHomepage} target="_blank">
-            <TbWorldWww />
-          </a>
-        )}
-        {props.topics &&
-          props.topics.map((topic: string) => {
-            return (
-              <Tag.Root key={topic}>
-                <Tag.Label>{topic}</Tag.Label>
-              </Tag.Root>
-            )
-          })}
-      </CardFooter>
+    <Card.Root h="100%">
+      <Box
+        as={'a'}
+        // @ts-ignore
+        href={props.liveHomepage ?? props.url}
+        target="_blank"
+        rel="noopener noreferrer"
+        transition="all 0.3s ease-in-out"
+        _hover={{
+          transform: 'translate(10px, -10px)', // Moves the card up slightly
+          boxShadow: 'xl' // Adds a stronger shadow for a lifting effect
+        }}
+        _focus={{ outline: 'none' }}
+        cursor="pointer"
+        h="100%">
+        <CardHeader
+          onClick={() => handleCardClick(props.url, props.liveHomepage)}>
+          <Flex gap={2} align={'center'}>
+            {langMappings[props?.language?.toLowerCase()]}
+            {props?.language}
+            <Flex ml="auto" gap={2} align={'center'}>
+              <a
+                href={props.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                onClick={(e) => e.stopPropagation()}>
+                <Box
+                  as={FaGithub}
+                  transition="all 0.2s ease-in-out"
+                  boxSize={4}
+                  _hover={{ color: 'blue.500', transform: 'scale(1.2)' }}
+                />
+              </a>
+              {props.liveHomepage && (
+                <a
+                  href={props.liveHomepage}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onClick={(e) => e.stopPropagation()}>
+                  <Box
+                    as={TbWorldWww}
+                    transition="all 0.2s ease-in-out"
+                    boxSize={5}
+                    _hover={{ color: 'blue.500', transform: 'scale(1.2)' }}
+                  />
+                </a>
+              )}
+            </Flex>
+          </Flex>
+        </CardHeader>
+        <CardBody
+          onClick={() => handleCardClick(props.url, props.liveHomepage)}>
+          <CardTitle>{props.name}</CardTitle>
+          <CardDescription>{props.description}</CardDescription>
+        </CardBody>
+        <CardFooter>
+          {props.topics &&
+            props.topics.map((topic: string) => {
+              return (
+                <Tag.Root key={topic}>
+                  <Tag.Label>{topic}</Tag.Label>
+                </Tag.Root>
+              )
+            })}
+        </CardFooter>
+      </Box>
     </Card.Root>
   )
 }
