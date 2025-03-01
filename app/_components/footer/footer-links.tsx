166dfc6ddf813bdc7cb2ca3a26a37e141cb59778
Collin Kleest <collinkleest@gmail.com>
2025-02-28 19:19:46 -0500

    feat(accessibility): add title to links Refs #9

--- app/_components/footer/footer-links.tsx
+++ app/_components/footer/footer-links.tsx
@@ -6,24 +6,37 @@ import { TfiEmail } from 'react-icons/tfi'
 export const FooterLinks = () => {
   return (
     <Box display={'flex'} gap={4}>
-      <a href="https://github.com/collinkleest" target="_blank">
+      <a
+        href="https://github.com/collinkleest"
+        target="_blank"
+        title="Github profile link">
         <FaGithub />
       </a>
 
-      <a href="https://linkedin.com/in/collinkleest" target="_blank">
+      <a
+        href="https://linkedin.com/in/collinkleest"
+        target="_blank"
+        title="Linkedin profile link">
         <FaLinkedin />
       </a>
 
-      <a href="https://dev.to/collinkleest" target="_blank">
+      <a
+        href="https://dev.to/collinkleest"
+        target="_blank"
+        title="Dev.to profile link">
         <FaDev />
       </a>
 
       <a
         href="https://open.spotify.com/user/1270362442?si=cc668d077a924196"
-        target="_blank">
+        target="_blank"
+        title="Spotify profile link">
         <FaSpotify />
       </a>
-      <a href="mailto:collinkleest@gmail.com" target="_blank">
+      <a
+        href="mailto:collinkleest@gmail.com"
+        target="_blank"
+        title="Collin Kleest email link">
         <TfiEmail />
       </a>
     </Box>
