9eb1ffefea8ea9ed3c21760cce05a161c33ecd55
Collin Kleest <collinkleest@gmail.com>
2025-03-06 21:37:38 -0500

    feat(analytics): abstract cta event

--- app/_utils/analytics.ts
+++ app/_utils/analytics.ts
@@ -1,9 +1,22 @@
 import { sendGAEvent } from '@next/third-parties/google'
 
+export interface ICallToActionEvent {
+  ctaTitle: string
+  ctaText?: string
+  ctaHref?: string
+}
+
 export const sentAnalyticEvent = (
   value: object,
   type = 'event',
   action = 'buttonClicked'
 ) => {
   sendGAEvent(type, action, value)
+}
+
+export const sendCallToActionAnalyticEvent = (
+  event: ICallToActionEvent,
+  action = 'buttonClicked'
+) => {
+  sentAnalyticEvent(event, action)
 }
