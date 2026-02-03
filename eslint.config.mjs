ad6efc49fcc37f641533a17ab75dcc2666d502c3
Collin Kleest <collinkleest@gmail.com>
2026-02-02 23:10:57 -0600

    feat: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,17 @@
 import eslint from '@eslint/js'
+import eslintReact from 'eslint-plugin-react'
 import { defineConfig, globalIgnores } from 'eslint/config'
 import tseslint from 'typescript-eslint'
 
 const eslintConfig = defineConfig([
   eslint.configs.recommended,
   tseslint.configs.recommended,
+  eslintReact.configs.flat.recommended,
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off'
+    }
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
