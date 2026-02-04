1bd243869264d79d7849e3cc1947da62568fdb5c
Collin Kleest <collinkleest@gmail.com>
2026-02-04 00:51:31 -0600

    feat: content updates

--- app/_types/experience.types.ts
+++ app/_types/experience.types.ts
@@ -3,6 +3,7 @@ import { ReactElement } from 'react'
 export interface IExperienceRole {
   roleTitle: string
   roleTimeline?: string
+  roleLocation?: string
   roleBlurb?: string
   roleBullets: string[]
   avatar?: ReactElement
@@ -14,6 +15,7 @@ export interface IExperienceBlock {
   roles: IExperienceRole[]
   avatar?: ReactElement
   connector?: ReactElement
+  location?: string
 }
 
 export interface IExperience {
